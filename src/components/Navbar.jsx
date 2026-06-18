333a1a065ffe8430caf2336fb64d3cc0995e88c1
brent <brentixorgeix@gmail.com>
2026-06-18 12:58:32 +0800

    fix: navbar scroll threshold set to 400px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
   const isHome = location.pathname === '/'
 
   useEffect(() => {
-    const onScroll = () => setScrolled(window.scrollY > 300)
+    const onScroll = () => setScrolled(window.scrollY > 400)
     window.addEventListener('scroll', onScroll)
     return () => window.removeEventListener('scroll', onScroll)
   }, [])
